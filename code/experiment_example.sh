e3908e2cfc3954e05df7f80dc120a9ac707f7005
lowe <diegocorrea.cc@gmail.com>
2023-05-16 17:28:06 -0300

    novos experimentos

--- code/experiment_example.sh
+++ code/experiment_example.sh
@@ -1,19 +1,39 @@
-python step1_preprocessing.py -opt=SPLIT --dataset=yahoo-movies
+python step1_preprocessing.py -opt=SPLIT --dataset=twitter_movies
 
-python step1_preprocessing.py -opt=DISTRIBUTION --dataset=yahoo-movies --distribution=CWS
+python step1_preprocessing.py -opt=DISTRIBUTION --dataset=twitter_movies --distribution=CWS
 
-python step2_searches.py -opt=RECOMMENDER --recommender=SVD --dataset=yahoo-movies
+python step2_searches.py -opt=RECOMMENDER --recommender=SVD --dataset=twitter_movies
 
-python step2_searches.py -opt=CONFORMITY --recommender=SVD --dataset=yahoo-movies
+python step2_searches.py -opt=CONFORMITY --recommender=SVD --dataset=twitter_movies
 
-python step3_processing.py --dataset=yahoo-movies --recommender=SVD
+python step3_processing.py --dataset=twitter_movies --recommender=SVD
 
-python step4_postprocessing.py --dataset=yahoo-movies --recommender=SVD --relevance=NDCG --calibration=VICIS_EMANON2 --tradeoff=LIN --distribution=CWS
+python step4_postprocessing.py --dataset=twitter_movies --recommender=SVD --relevance=NDCG --calibration=VICIS_EMANON2 --tradeoff=LIN --distribution=CWS
 
-python step5_metrics.py -opt=RECOMMENDER --dataset=yahoo-movies --recommender=SVD --distribution=CWS --relevance=NDCG --calibration=VICIS_EMANON2 --tradeoff=LIN --selector=SURROGATE
+python step5_metrics.py -opt=RECOMMENDER --dataset=twitter_movies --recommender=SVD --distribution=CWS --relevance=NDCG --calibration=VICIS_EMANON2 --tradeoff=LIN --selector=SURROGATE
 
-python step5_metrics.py -opt=CONFORMITY --dataset=yahoo-movies --recommender=SVD --distribution=CWS --relevance=NDCG --calibration=VICIS_EMANON2 --tradeoff=LIN --selector=SURROGATE
+python step5_metrics.py -opt=CONFORMITY --dataset=twitter_movies --recommender=SVD --distribution=CWS --relevance=NDCG --calibration=VICIS_EMANON2 --tradeoff=LIN --selector=SURROGATE
 
-python step6_protocol.py -opt=RECOMMENDER --dataset=yahoo-movies --recommender=SVD --distribution=CWS --relevance=NDCG --calibration=VICIS_EMANON2 --tradeoff=LIN --selector=SURROGATE
+python step6_protocol.py -opt=RECOMMENDER --dataset=twitter_movies --recommender=SVD --distribution=CWS --relevance=NDCG --calibration=VICIS_EMANON2 --tradeoff=LIN --selector=SURROGATE
 
-python step6_protocol.py -opt=CONFORMITY --dataset=yahoo-movies --recommender=SVD --distribution=CWS --relevance=NDCG --calibration=VICIS_EMANON2 --tradeoff=LIN --selector=SURROGATE
+python step6_protocol.py -opt=CONFORMITY --dataset=twitter_movies --recommender=SVD --distribution=CWS --relevance=NDCG --calibration=VICIS_EMANON2 --tradeoff=LIN --selector=SURROGATE
+
+python step1_preprocessing.py -opt=SPLIT --dataset=ml-1m
+
+python step1_preprocessing.py -opt=DISTRIBUTION --dataset=ml-1m --distribution=CWS
+
+python step2_searches.py -opt=RECOMMENDER --recommender=SVD --dataset=ml-1m
+
+python step2_searches.py -opt=CONFORMITY --recommender=SVD --dataset=ml-1m
+
+python step3_processing.py --dataset=ml-1m --recommender=SVD
+
+python step4_postprocessing.py --dataset=ml-1m --recommender=SVD --relevance=NDCG --calibration=VICIS_EMANON2 --tradeoff=LIN --distribution=CWS
+
+python step5_metrics.py -opt=RECOMMENDER --dataset=ml-1m --recommender=SVD --distribution=CWS --relevance=NDCG --calibration=VICIS_EMANON2 --tradeoff=LIN --selector=SURROGATE
+
+python step5_metrics.py -opt=CONFORMITY --dataset=ml-1m --recommender=SVD --distribution=CWS --relevance=NDCG --calibration=VICIS_EMANON2 --tradeoff=LIN --selector=SURROGATE
+
+python step6_protocol.py -opt=RECOMMENDER --dataset=ml-1m --recommender=SVD --distribution=CWS --relevance=NDCG --calibration=VICIS_EMANON2 --tradeoff=LIN --selector=SURROGATE
+
+python step6_protocol.py -opt=CONFORMITY --dataset=ml-1m --recommender=SVD --distribution=CWS --relevance=NDCG --calibration=VICIS_EMANON2 --tradeoff=LIN --selector=SURROGATE
